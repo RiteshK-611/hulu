cbc92fc0b216f67b4da05b2c212124d3c0782d1b
Ritesh <riteshkokam@gmail.com>
2022-07-30 13:40:25 +0530

    Add Flip Card for Overview

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -6,24 +6,32 @@ const Thumbnail = forwardRef(({ result }, ref) => {
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
 
   return (
-    <div
-      ref={ref}
-      className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50"
-    >
-      <Image
-        className="rounded-lg"
-        src={
-          `${BASE_URL}${result.backdrop_path || result.poster_path}` ||
-          `${BASE_URL}${result.poster_path}`
-        }
-        alt="movie-poster"
-        layout="responsive"
-        height={1080}
-        width={1920}
-      />
-
+    <div className="group">
+      <div ref={ref}
+        className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50 flip-card">
+        <div className="flip-card-inner">
+          <div className="flip-card-front">
+          <Image
+            className="rounded-lg"
+            src={
+              `${BASE_URL}${result.backdrop_path || result.poster_path}` ||
+              `${BASE_URL}${result.poster_path}`
+            }
+            alt="movie-poster"
+            layout="fill"
+            // height={1080} // require for responsive layout 268.5
+            // width={1920} // require for responsive layout 477.34
+            // onClick={() => console.log("clicked image")}
+          />
+          </div>
+          <div className="flip-card-back">
+            <h1>Overview:</h1> 
+            <p>{result.overview}</p>
+          </div>
+        </div>
+      </div>
       <div className="p-2">
-        <p className="truncate max-w-md">{result.overview}</p>
+        {/* <p className="truncate max-w-md">{result.overview}</p> */}
         <h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">
           {result.title || result.original_name}
         </h2>
@@ -34,9 +42,39 @@ const Thumbnail = forwardRef(({ result }, ref) => {
         </p>
       </div>
     </div>
+
+    // <div
+    //   ref={ref}
+    //   className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50"
+    // >
+    //   <Image
+    //     className="rounded-lg"
+    //     src={
+    //       `${BASE_URL}${result.backdrop_path || result.poster_path}` ||
+    //       `${BASE_URL}${result.poster_path}`
+    //     }
+    //     alt="movie-poster"
+    //     layout="responsive"
+    //     height={1080}
+    //     width={1920}
+    //     onClick={() => console.log("clicked image")}
+    //   />
+
+    //   <div className="p-2">
+    //     <p className="truncate max-w-md">{result.overview}</p>
+    //     <h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">
+    //       {result.title || result.original_name}
+    //     </h2>
+    //     <p className="flex items-center opacity-0 group-hover:opacity-100 uppercase">
+    //       {result.media_type && `${result.media_type} •`}{" "}
+    //       {result.release_date || result.first_air_data} •{" "}
+    //       <ThumbUpIcon className="h-5 mx-2" /> {result.vote_count}
+    //     </p>
+    //   </div>
+    // </div>
   );
 });
 
-Thumbnail.displayName = 'thumbnail';
+Thumbnail.displayName = "thumbnail";
 
 export default Thumbnail;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,5 @@
-const plugin = require("tailwindcss/plugin");
-
-const Rotate = plugin(function ({ addUtilities }) {
-  addUtilities({
-    ".rotate-y-180": {
-      transform: "rotateY(180deg)",
-    },
-    ".preserve-3d": {
-      transformStyle: "preserve-3d"
-    },
-    ".perspective": {
-      perspective: "1000px"
-    },
-    ".backface-hidden": {
-      backfaceVisibility: "hidden"
-    }
-  });
-});
+const plugin = require("tailwindcss/plugin")
+// require("tailwind-scrollbar-hide"),
 
 module.exports = {
   mode: "jit",
@@ -30,5 +14,58 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwind-scrollbar-hide"), Rotate],
-};
+  plugins: [
+    require("tailwind-scrollbar-hide"),
+    plugin(function({ addUtilities }) {
+      addUtilities({     
+        '.flip-card': {
+          backgroundColor: 'transparent',
+          width: '490px', //490=474, 495=479 diff. of 24
+          height: '290px', //285=269, 290=274, 295=279
+          perspective: '1000px',
+          '&:hover': {
+            '.flip-card-inner': {
+              transform: 'rotateY(180deg)'
+            }
+          }
+        },
+        '.flip-card-inner': {
+          position: 'relative',
+          width: '100%',
+          height: '100%',
+          textAlign: 'center',
+          transition: 'transform 3s',
+          transformStyle: 'preserve-3d',
+          boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)',
+        },
+        '.flip-card-front': {
+          position: 'absolute',
+          width: '100%',
+          height: '100%',
+          '-webkit-backface-visibility': 'hidden',
+          backfaceVisibility: 'hidden',
+          backgroundColor: '#bbb',
+          color: 'black',
+          borderRadius: '0.5rem',
+        },
+        '.flip-card-back': {      // brings x-axis scroll bar on hovering last column element
+          position: 'absolute',
+          width: '100%',
+          height: '100%',
+          '-webkit-backface-visibility': 'hidden',
+          backfaceVisibility: 'hidden',
+          backgroundColor: '#06202A',
+          // backgroundColor: '#2980b9',
+          backgroundImage: 'url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/308367/fair.jpg)',
+          color: 'white',
+          transform: 'rotateY(180deg)',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          borderRadius: '0.5rem',
+          padding: '1rem 2rem',
+        }
+      })
+    })
+  ],
+}
